7bd61c7f31521d20db0bed602e5e344a48db1975
alt.muhle@gmail.com <36291373+YeOldeMiller@users.noreply.github.com>
2019-01-25 09:38:00 +0300

    Finish Section 4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Provider } from './context';
+import Contacts from './components/contacts/ContactList';
+import Header from './components/layout/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Provider>
+                <div className="App">
+                    <Header branding="Contact Manager" />
+                    <div className="container">
+                        <Contacts />
+                    </div>
+                </div>
+            </Provider>
+        );
+    }
 }
 
 export default App;
+    

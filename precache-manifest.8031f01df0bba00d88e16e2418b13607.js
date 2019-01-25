db26fbf3c3ce2be797056cf34db450b9a5e53a64
YeOldeMiller <alt.muhle@gmail.com>
2019-01-25 22:15:03 +0300

    Updates

---
+++ precache-manifest.8031f01df0bba00d88e16e2418b13607.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe7f281c9d4a39ecb49e",
+    "url": "/react-contact-manager/static/css/main.34a994d1.chunk.css"
+  },
+  {
+    "revision": "fe7f281c9d4a39ecb49e",
+    "url": "/react-contact-manager/static/js/main.fe7f281c.chunk.js"
+  },
+  {
+    "revision": "1024808f21fce56b0597",
+    "url": "/react-contact-manager/static/css/1.6bd5d8b9.chunk.css"
+  },
+  {
+    "revision": "1024808f21fce56b0597",
+    "url": "/react-contact-manager/static/js/1.1024808f.chunk.js"
+  },
+  {
+    "revision": "ac3c48b9cdbff86df9e4",
+    "url": "/react-contact-manager/static/js/runtime~main.ac3c48b9.js"
+  },
+  {
+    "revision": "55c883b3de889045fe07d3364e49e607",
+    "url": "/react-contact-manager/index.html"
+  }
+];
